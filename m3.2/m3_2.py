cd2a2176ba48b8ba968368f5b42aa8497cf40982
ANISH-KARTHIC <24uad110anish@kgkite.ac.in>
2025-08-28 09:43:00 +0530

    Update m3_2.py

--- m3.2/m3_2.py
+++ m3.2/m3_2.py
@@ -1,3 +1,24 @@
 
 def list_third_element():
     #Your creative code goes here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #do not  edit below this line
+if __name__ == "__main__":
+   list_third_element()
