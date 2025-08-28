8e849a27b4bb415c24b9e0ec602d41f7e5f8ab8b
ANISH-KARTHIC <24uad110anish@kgkite.ac.in>
2025-08-28 09:40:21 +0530

    Update m3_1.py

--- m3.1/m3_1.py
+++ m3.1/m3_1.py
@@ -1,2 +1,23 @@
 def compare_with_five():
     #Your creative code goes here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #do not edit below this line
+if __name__ == "__main__":
+   compare_with_five()
+    
